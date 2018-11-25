762b113c6415d8602c2a267720c4a7d66d358279
Reto Lamprecht <reto.lamprecht@gmx.ch>
2018-11-25 19:48:58 +0100

    basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development', // 'production' | 'development' | 'none'
+    entry: __dirname + '/src/app/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: [
+                    /node_modules/
+                ]
+            },
+            {
+                test: /\.html/,
+                loader: 'raw-loader'
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            template: __dirname + '/src/public/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: './src/public',
+        port: 7700,
+    }
+};
